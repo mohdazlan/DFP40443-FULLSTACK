56d08b0b15c7b992574a648bbeed613b4dbcea9e
Mohd Azlan <mhdazlanabaziz@gmail.com>
2026-01-20 15:16:56 +0800

    third commit

--- W2/play.php
+++ W2/play.php
@@ -2,4 +2,7 @@
 $name = "Carlson";
 echo "<h1>Hello World</h1>  $name";
 echo 'This is a full stack class'.$name;
+echo "<h1> My name is Azlan</h1>";
+echo "<h2> My PA name is Azlan</h2>";
+echo "<h3> My department name is Azlan</h3>";
 ?>
