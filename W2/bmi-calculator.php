742bedb1a1c188bfbe1af174687f2ac1f9f6fd9e
Mohd Azlan <mhdazlanabaziz@gmail.com>
2026-01-22 11:15:28 +0800

    tambah file bmi calculator

---
+++ W2/bmi-calculator.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+</head>
+<body>
+   
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+<label>Height (cm)</label>
+<input name="height">
+<label>Weight (kg)</label>
+<input name="weight">
+<input type="submit" value="Calculate">
+</form>
+</body>
+    </html>
+
