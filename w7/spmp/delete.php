fc64d29a3116d12e6fa4053be2ecdb5badfa4ca7
Mohd Azlan <mhdazlanabaziz@gmail.com>
2026-03-05 11:02:06 +0800

    added delete

--- w7/spmp/delete.php
+++ w7/spmp/delete.php
@@ -1,6 +1,21 @@
 <?php
 require_once("config/app_config.php");
 $maklumat = mysqli_query($conn, "SELECT users.id,username as pengguna,email,password,name as peranan FROM spmp.users join roles on users.role_id = roles.id;") or die(mysqli_error($conn));
+
+$mesej ="";
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $userid = $_POST["user_id"];
+
+    $stmt = mysqli_prepare($conn,"DELETE FROM users WHERE id=?");
+    mysqli_stmt_bind_param($stmt,"i", $userid);
+
+    if(mysqli_stmt_execute($stmt)){
+        $mesej = "<p style='color:green'>User succssfully deleted!</p>";
+    }else{
+         $mesej = "<p style='color:red'>User not succssfully deleted!</p>";
+       // echo "". mysqli_error($conn);
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,8 +27,9 @@ $maklumat = mysqli_query($conn, "SELECT users.id,username as pengguna,email,pass
 </head>
 
 <body>
+    <?php echo $mesej; ?>
     <h2>Delete User</h2>
-    <table>
+    <table border="1">
         <tr>
             <th>Id</th>
             <th>Nama Pengguna</th>
@@ -26,7 +42,12 @@ $maklumat = mysqli_query($conn, "SELECT users.id,username as pengguna,email,pass
                 <td><?php echo $pengguna['id']; ?></td>
                 <td><?php echo $pengguna['pengguna']; ?></td>
                 <td><?php echo $pengguna['peranan']; ?></td>
-                <td><input type="submit" value="Padam"></td>
+                <td>
+                    <form method="POST" action="">
+                        <input type="hidden" name="user_id" value="<?php echo $pengguna['id']; ?>">
+                        <input type="submit" value="Padam">
+                    </form>
+                </td>
                 </tr>
             <?php endwhile; ?>
 
