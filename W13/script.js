3ebcf2fb07efda906f84847896476c4717a0ff06
Mohd Azlan <mhdazlanabaziz@gmail.com>
2026-04-14 15:19:12 +0800

    count users

--- W13/script.js
+++ W13/script.js
@@ -1,7 +1,36 @@
-document.getElementById("loadMessage").addEventListener("click", function () {
-    fetch("message.php")
-        .then(res => res.text())
-        .then(data => {
-            document.getElementById("result").innerHTML = data;
-        });
-});
+const showBtn = document.getElementById("loadMessage");
+const sysBtn = document.getElementById("loadSystem");
+const SQLBtn = document.getElementById("checkDB");
+
+if (SQLBtn) {
+   SQLBtn.addEventListener("click", function () {
+        fetch("count.php")
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById("result").innerHTML = "<p style='color:red'>" + data + "<p>";
+            })
+    });
+}
+
+if (showBtn) {
+    showBtn.addEventListener("click", function () {
+        fetch("message.php")
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById("result").innerHTML = "<p style='color:red'>" + data + "<p>";
+            })
+    });
+}
+
+if (sysBtn) {
+    sysBtn.addEventListener("click", function () {
+        fetch("message.php")
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById("result").innerHTML = "<p>" + data + "<p>";
+            })
+    });
+}
+
+
+
